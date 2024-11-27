4ddd1995dc6cfb8991445cd0f0f95c751d3e3483
n ghtcjm <84369321+choi138@users.noreply.github.com>
2024-11-27 20:16:33 +0900

    refactor: change folder structure

--- src/components/Locations/index.tsx
+++ src/components/Locations/index.tsx
@@ -1,11 +1,11 @@
 import {useQuery} from '@apollo/client';
 import React from 'react';
 import {Text} from 'react-native';
-import {getLocations} from '../../GetLocations';
-import {GetLocationsQuery} from '../../types';
+import {getLocationsQueryDocument} from '../../queries/GetLocations';
+import {GetLocationsQuery} from '../../gql/types';
 
 export const Locations: React.FC = () => {
-  const {error, data} = useQuery<GetLocationsQuery>(getLocations);
+  const {error, data} = useQuery<GetLocationsQuery>(getLocationsQueryDocument);
   console.log(data, 'data');
 
   if (error) {
--- src/queries/GetLocations.ts
+++ src/queries/GetLocations.ts
@@ -1,6 +1,6 @@
 import {gql} from '@apollo/client';
 
-export const getLocations = gql`
+export const getLocationsQueryDocument = gql`
   query GetLocations {
     locations {
       id
